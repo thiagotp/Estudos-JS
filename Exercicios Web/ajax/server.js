4a6d759309bad372d531a72a7cb9e0cc7d1091f3
thiagotp <thiagoteixeira.ps@gmail.com>
2022-04-05 22:38:28 -0300

    Consumindo um arquivo XML com ajax

--- Exercicios Web/ajax/server.js
+++ Exercicios Web/ajax/server.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
+// interpretar o aquivo que vem do upload
+const multer = require('multer')
 
 app.use(express.static('.'))
 // Se o formato vier for a partir de um submit usará bodyParser.urlencoded({ extended: true})
@@ -9,6 +11,28 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // Se o formato que vier for um JSON, usará bodyParser.json()
 app.use(bodyParser.json())
 
+// configurando a pasta que o arquivo vai ficar e o nome do arquivo
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        // o segundo parâmetro é a pasta destino do arquivo
+        callback(null, './upload')
+    },
+    // função para selecionar o nome do arquivo
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end("Ocorreu um erro")
+        }
+        res.end('Concluído com sucesso.')
+    })
+})
 
 // Dando start no servidor
 app.listen(8080, () => console.log('Executando ....'))
