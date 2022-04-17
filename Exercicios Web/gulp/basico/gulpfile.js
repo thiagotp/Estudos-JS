0dbf795871b58bc77a00e145f5b5819f140d847f
thiagotp <thiagoteixeira.ps@gmail.com>
2022-04-17 16:41:38 -0300

    iniciando estudos de gulp

---
+++ Exercicios Web/gulp/basico/gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp')
+const series = gulp.series
+// ou const { series, parallel } = require('gulp')
+
+const antes1 = cb => {
+    console.log('Tarefa antes 1')
+    return cb()
+}
+
+const antes2 = cb => {
+    console.log('Tarefa antes 2')
+    return cb()
+}
+
+function copiar(callback) {
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt']) ou
+    gulp.src(['pastaA/**/*.txt'])
+        .pipe(gulp.dest('pastaB'))
+    return callback()
+}
+
+const fim = cb => {
+    console.log('Tarefa Fim')
+    return cb()
+}
+
+
+module.exports.default = series(
+    antes1,
+    antes2,
+    copiar,
+    fim
+)
