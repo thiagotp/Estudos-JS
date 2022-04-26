02cbd55fe7b354e3ea9ab022d52cf2dc62c9e60b
thiagotp <thiagoteixeira.ps@gmail.com>
2022-04-25 21:31:13 -0300

    Continuando com webpack (carregando e externalizando css)

--- Exercicios Web/webpack/webpack.config.js
+++ Exercicios Web/webpack/webpack.config.js
@@ -1,10 +1,26 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     mode: "development",
     entry: "./src/principal.js",
-    output:{
+    output: {
         filename: "principal.js",
         path: __dirname + '/public'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                // 'style-loader', //Adiciona CSS a DOM injetando a tag <style>
+                'css-loader' //interpreta @import, url()...
+            ]
+        }]
     }
 }
