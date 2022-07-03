7e259c545e42be13af5c012cf1e28bf741ef9bc0
thiagotp <thiagoteixeira.ps@gmail.com>
2022-07-03 18:05:03 -0300

    Começando estudos de React (componentes)

---
+++ Exercicios Web/react/exercicios/src/components/BomDia.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default (props) => {
+    return <React.Fragment>
+        <h1>Bom Dia {props.nome}!</h1>
+        <h2>Aprendedo</h2>
+    </React.Fragment>
+}
---
+++ Exercicios Web/react/exercicios/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Primeiro from './components/Primeiro'
+import BomDia from './components/BomDia'
+
+ReactDOM.render(<BomDia nome="Thiago" />, document.getElementById('root'))
